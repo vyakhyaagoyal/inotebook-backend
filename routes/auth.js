5f1c4697875fc606dd0205bca8de7a09924b698f
Vyakhya Goyal <vyakhyatwentytwo@gmail.com>
2025-06-07 16:16:27 +0530

    express-validator(querystring & requestbody) done

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,20 @@
-const express=require('express');
+const express = require('express');
 const User = require('../models/User');
-const router=express.Router();  //express router object
+const router = express.Router();  //express router object
+const { query,body, validationResult } = require('express-validator');
 
-router.post('/', (req,res)=>{
-    res.send(req.body);
-    const user=new User(req.body);
-    user.save();
-})
+router.post('/', [body('email', 'enter a valid mail').isEmail(),
+    body('name','length is less than 3').isLength({min:3})], (req, res) => {
+    // res.send(req.body);
+    // const user=new User(req.body);
+    // user.save();
 
-module.exports=router
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+        return res.send(req.body); //send object directly or use JSON.stringify
+    }
+
+    res.send({ errors: result.array() });
+});
+
+module.exports = router
