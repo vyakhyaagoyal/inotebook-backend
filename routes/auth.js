e559c91c87bfd7729d9105f11d9fdeac3d695528
Vyakhya Goyal <vyakhyatwentytwo@gmail.com>
2025-06-08 19:42:33 +0530

    collections managed

--- routes/auth.js
+++ routes/auth.js
@@ -3,17 +3,17 @@ const User = require('../models/User');
 const router = express.Router();  //express router object
 const { query, body, validationResult } = require('express-validator');
 
-//create a user using POST
+//create a user using POST: "/api/auth/createuser"
 router.post('/createuser', [body('email', 'enter a valid mail').isEmail(),
 body('name', 'length is less than 3').isLength({ min: 3 }),
 body('password', 'password must be atleast 5 characters').isLength({ min: 5 }),
 body('password', 'enter valid password').isStrongPassword({ minLength: 5, })],
-    // default settings of isStrongPassword()=> { minLength: 8, minLowercase: 1, minUppercase: 1, 
+    // default settings/parameters of isStrongPassword()=> { minLength: 8, minLowercase: 1, minUppercase: 1, 
     // minNumbers: 1, minSymbols: 1, returnScore: false, 
     // pointsPerUnique: 1, pointsPerRepeat: 0.5, 
     // pointsForContainingLower: 10, pointsForContainingUpper: 10, 
     // pointsForContainingNumber: 10, pointsForContainingSymbol: 10 }
-    (req, res) => {
+    async (req, res) => {
         // res.send(req.body);
         // const user=new User(req.body);
         // user.save();
@@ -23,12 +23,26 @@ body('password', 'enter valid password').isStrongPassword({ minLength: 5, })],
             return res.status(400).send({ errors: result.array() });
         }
 
-        User.create({
-            name:req.body.name,
-            email:req.body.email,
-            password:req.body.password
-        }).then(user=>res.json({name: user.name, email: user.email, id: user._id }))
-        .catch(err => res.status(500).json({ error: 'Internal Server Error', details: err }));
+        try {
+            //check if this email already exists
+            let user = await User.findOne({ email: req.body.email });
+            if (user) {
+                return res.status(400).json({ error: "User with this email already exists" })
+            }
+            user = await User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            })
+            res.json({ name: user.name, email: user.email, id: user._id })
+        }
+        catch (error) {
+            console.error(error.message);
+            res.status(500).send("Error occurred");
+        }
+
+        // .then(user=>res.json({name: user.name, email: user.email, id: user._id }))
+        // .catch(err => res.status(500).json({ error: 'Enter unique email', details: err }));
 
         // res.send(req.body); //send object directly or use JSON.stringify
     });
